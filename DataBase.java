cc70903a40d904865f9fb4f4505a609092af4ad0
Lolita <lolita.11.07.06@gmail.com>
2024-06-07 19:00:51 +0700

    Add insert function to DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,13 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DataBase {
-    private Connection con;
+    private final Connection con;
 
-    DataBase() {
+    public DataBase() {
         try {
             con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/base1", "username", "pass");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insert(String name, String id) {
+        try (PreparedStatement preSt = con.prepareStatement("insert into game (name_, id) values (?, ?)")) {
+            preSt.setString(1, name);
+            preSt.setString(1, id);
+            System.out.print(preSt.getUpdateCount());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
