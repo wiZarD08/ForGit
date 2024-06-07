c10d25f3ec1fc0059f40ddb4d3d084f463758cef
Lolita <lolita.11.07.06@gmail.com>
2024-06-07 19:29:46 +0700

    Optimize imports

--- DataBase.java
+++ DataBase.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DataBase {
     private final Connection con;
