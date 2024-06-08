46425acc55bedd319d7c3fd4a8bddfa2bc140ace
Lolita <lolita.11.07.06@gmail.com>
2024-06-08 14:17:37 +0700

    Add delete fun

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         DataBase db = new DataBase();
         db.selectAll();
+        db.delete(1);
+        db.selectAll();
         db.closeCon();
     }
 
